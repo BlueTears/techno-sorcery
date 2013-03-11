eddeb3154fdfb1ebe1e3b98f287939e2abc79e35
BlueTears <coasterfanbase@gmail.com>
2013-03-11 01:50:17 -0400

    Why you no commit the first time Iron Mold

---
+++ common/com/bluetears/technosorcery/items/molds/IronMold.java
@@ -0,0 +1,12 @@
+package com.bluetears.technosorcery.items.molds;
+
+import net.minecraft.item.Item;
+
+public class IronMold extends Item {
+
+        public IronMold(int id) {
+                super(id);
+                // TODO Auto-generated constructor stub
+        }
+
+}
