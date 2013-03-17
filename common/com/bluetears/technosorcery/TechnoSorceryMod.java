1d08416dbba6ccf71569f53aa135be94c2044c86
BlueTears <maziarkj@my.erau.edu>
2013-03-17 01:57:56 -0400

    Added Silicate in game, Texture not working though?

--- common/com/bluetears/technosorcery/TechnoSorceryMod.java
+++ common/com/bluetears/technosorcery/TechnoSorceryMod.java
@@ -1,5 +1,11 @@
 package com.bluetears.technosorcery;
 
+
+
+import net.minecraft.item.Item;
+
+import com.bluetears.technosorcery.items.Silicate;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,6 +16,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid="TechnoSorcery", name="Techno Sorcery Mod", version="0.0.1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
@@ -18,6 +25,14 @@ public class TechnoSorceryMod {
         // The instance of your mod that Forge uses.
         @Instance("TechnoSorcery")
         public static TechnoSorceryMod instance;
+        
+        //Below is the code for generating all of the blocks
+        public final static Item silicate = new Silicate(5001).setMaxStackSize(16);
+        
+        
+        //Below is the code for generating all of the items
+        
+        
         
         // Says where the client and server 'proxy' code is loaded.
         @SidedProxy(clientSide="com.bluetears.technosorcery.client.ClientProxy", serverSide="com.bluetears.technosorcery.CommonProxy")
@@ -30,6 +45,7 @@ public class TechnoSorceryMod {
         
         @Init
         public void load(FMLInitializationEvent event) {
+        	LanguageRegistry.addName(silicate, "Silicate");
                 proxy.registerRenderers();
         }
         
@@ -37,4 +53,8 @@ public class TechnoSorceryMod {
         public void postInit(FMLPostInitializationEvent event) {
                 // Stub Method
         }
+        
+        
 }
+
+
--- common/com/bluetears/technosorcery/items/Silicate.java
+++ common/com/bluetears/technosorcery/items/Silicate.java
@@ -1,5 +1,22 @@
 package com.bluetears.technosorcery.items;
 
-public class Silicate {
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class Silicate extends Item
+{
+
+public Silicate(int id)
+{
+         super(id);
+         setCreativeTab(CreativeTabs.tabMisc);
+}
+
+@Override
+public void func_94581_a(IconRegister iconRegister)
+{
+         iconIndex = iconRegister.func_94245_a("techno-sorcery:silicate.png");
+}
 
 }
